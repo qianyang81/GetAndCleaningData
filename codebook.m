1768165d1c433ab2af145c414e11f345d7fe2ad4
qianyang81 <chilizzy@gmail.com>
2015-01-23 23:48:15 +0100

    code book file

---
+++ codebook.m
@@ -0,0 +1,11 @@
+Column Definitions
+
+subjectId: A unique integer with a range of 1 to 30 representing individual subjects
+activity: A factor with 6 levels translated from the original research. Levels:
+WALKING
+WALKING_UPSTAIRS
+WALKING_DOWNSTAIRS
+SITTING
+STANDING
+LAYING
+Mean of Measurements: The remaining 79 columns are the mean of selected smoothed sensor data produced by the original research. To enable transparency and direct lineage back to the original research the column names are taken from the features with the identical names in the original work. The pattern is explained in the section above.
